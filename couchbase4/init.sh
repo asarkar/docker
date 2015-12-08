5bb7b3ba1825599bf25aaa869d51e67dbf06b08c
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2015-12-07 21:18:41 -0800

    Finally Couchbase up and running

--- couchbase4/init.sh
+++ couchbase4/init.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
 
-$CB_HOME/etc/couchbase_init.d restart
-sleep 5
+isRunning() {
+	echo "$($CB_HOME/etc/couchbase_init.d status)" | grep not > /dev/null 2>&1
 
-echo "$($CB_HOME/etc/couchbase_init.d status)" | grep not > /dev/null 2>&1
+	return "$?"
+}
 
+rm -f /etc/init.d/couchbase-server
+$CB_HOME/etc/couchbase_init.d restart > /dev/null 2>&1
+
+DEFAULT_NUM_RETRY=5
+DEFAULT_RETRY_INTERVAL=2
+
+NUM_RETRY=${NUM_RETRY:-$(echo $DEFAULT_NUM_RETRY)}
+RETRY_INTERVAL=${RETRY_INTERVAL:-$(echo $DEFAULT_RETRY_INTERVAL)}
+
+COUNTER=$NUM_RETRY
+
+isRunning
+while [ "$?" -eq 0 -a $COUNTER -gt 0 ]; do
+    COUNTER=$((COUNTER - 1))
+
+    sleep $RETRY_INTERVAL
+done
+
+isRunning
 if [ "$?" -eq 0 ]; then
-	printf "\n[ERROR] Failed to start Couchbase server.\n"
+    printf "[ERROR] Failed to start Couchbase server after %d seconds.\n" $((NUM_RETRY * RETRY_INTERVAL))
 
-	exit 1
+    exit 1
 fi
 
 IFS=':' read -ra FIELDS <<< "$USER"
@@ -29,20 +49,31 @@ fi
 CB_USERNAME=${CB_USERNAME:-$(echo $DEFAULT_USERNAME)}
 CB_PASSWORD=${CB_PASSWORD:-$(echo $DEFAULT_PASSWORD)}
 
-ENDPOINT=http://localhost:8091
+ENDPOINT=http://127.0.0.1:8091
 
-printf "\n[INFO] Setting up admin credentials...\n"
-curl -d "username=$CB_USERNAME&password=$CB_PASSWORD&port=8091" $ENDPOINT/settings/web
+printf "\n[INFO] Setting up admin credentials.\n"
+curl -sSL -w "%{http_code} %{url_effective}\\n" -d username=$CB_USERNAME -d password=$CB_PASSWORD -d port=8091 $ENDPOINT/settings/web
 
-printf "\n[INFO] Configuring memory quota...\n"
-curl -u $CB_USERNAME:$CB_PASSWORD -d "memoryQuota=300&indexMemoryQuota=300" $ENDPOINT/pools/default
+printf "\n[INFO] Configuring memory quota.\n"
+curl -sSL -w "%{http_code} %{url_effective}\\n" -u $CB_USERNAME:$CB_PASSWORD -d memoryQuota=256 -d indexMemoryQuota=256 $ENDPOINT/pools/default
 
-if [ "$TRAVEL_SAMPLE" == "true" ]; then
-	printf "\n[INFO] Installing travel-sample...\n"
-	curl -u $CB_USERNAME:$CB_PASSWORD -d '["travel-sample"]' $ENDPOINT/sampleBuckets/install
+if [ -n "$SAMPLE_BUCKETS" ]; then
+	IFS=',' read -ra BUCKETS <<< "$SAMPLE_BUCKETS"
+
+	for bucket in "${BUCKETS[@]}"; do
+		printf "\n[INFO] Installing %s.\n" "$bucket"
+		curl -sSL -w "%{http_code} %{url_effective}\\n" -u $CB_USERNAME:$CB_PASSWORD --data-ascii '["'"$bucket"'"]' $ENDPOINT/sampleBuckets/install
+	done
 fi
 
-tail -f /etc/hosts
+tail -f <(cat <<EOF
+
+**************************************
+	Couchbase server is running...
+**************************************
+
+EOF
+)
 
 
 
